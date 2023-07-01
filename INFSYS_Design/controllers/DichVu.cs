439cc7e84fa42821eb2b65895d624c287400f864
hqthai <99537336+hqthai@users.noreply.github.com>
2023-07-01 22:18:15 +0700

    DichVu

---
+++ INFSYS_Design/controllers/DichVu.cs
@@ -0,0 +1,48 @@
+﻿using INFSYS_Design.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INFSYS_Design.controllers
+{
+    class DichVu
+    {
+        public int ma;
+        public string tenDichVu;
+        public string moTa;
+        public string loaiDichVu;
+        public int gia;
+        
+        public DichVu(Dictionary<string, object> kwargs)
+        {
+            object data;
+
+            if (kwargs.TryGetValue("MA", out data))
+            {
+                this.ma = int.Parse(data.ToString());
+            }
+            if (kwargs.TryGetValue("TENDICHVU", out data))
+            {
+                this.tenDichVu = data.ToString();
+            }
+            if (kwargs.TryGetValue("MOTA", out data))
+            {
+                this.moTa = data.ToString();
+            }
+            if(kwargs.TryGetValue("LOAIDICHVU", out data))
+            {
+                this.loaiDichVu = data.ToString();
+            }
+            if (kwargs.TryGetValue("GIA", out data))
+            {
+                this.gia = int.Parse(data.ToString());
+            }
+        }
+        public static List<DichVu> layDanhSachDichVu()
+        {
+            return DB_DichVu.layDanhSachDichVu();
+        }
+    }
+}
---
+++ INFSYS_Design/models/DB_DichVu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFSYS_Design.controllers;
+
+namespace INFSYS_Design.models
+{
+    class DB_DichVu
+    {
+        public static List<DichVu> layDanhSachDichVu()
+        {
+            DBConn conn = new DBConn();
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = System.Data.CommandType.Text;
+            sqlCmd.CommandText = $"SELECT * FROM DICHVU";
+            sqlCmd.Connection = conn.conn;
+            string[] columnNames = { "ma", "tenDichVu", "moTa", "loaiDichVu", "gia" };
+
+            SqlDataReader res = sqlCmd.ExecuteReader();
+
+            List<DichVu> list_of_services = new List<DichVu>();
+
+            while (res.Read())
+            {
+                Dictionary<string, object> raw_service = new Dictionary<string, object>();
+                foreach (string colName in columnNames)
+                {
+                    raw_service.Add(colName.ToUpper(), res.GetValue(res.GetOrdinal(colName.ToUpper())));
+                }
+                DichVu service = new DichVu(raw_service);
+                list_of_services.Add(service);
+            }
+            return list_of_services;
+        }
+    }
+}
