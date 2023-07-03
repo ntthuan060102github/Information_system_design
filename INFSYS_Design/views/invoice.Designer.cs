b6b7f9d43d9b176226f79f2c5051c881ea3cd8e5
hqthai <99537336+hqthai@users.noreply.github.com>
2023-07-03 18:24:13 +0700

    commit

--- INFSYS_Design/views/invoice.Designer.cs
+++ INFSYS_Design/views/invoice.Designer.cs
@@ -44,12 +44,14 @@
             this.lb_change = new System.Windows.Forms.Label();
             this.lb_change_text = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.lb_checkout_id_text = new System.Windows.Forms.Label();
+            this.lb_checkout_id = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // pay_btn
             // 
             this.pay_btn.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pay_btn.Location = new System.Drawing.Point(454, 464);
+            this.pay_btn.Location = new System.Drawing.Point(452, 488);
             this.pay_btn.Name = "pay_btn";
             this.pay_btn.Size = new System.Drawing.Size(97, 29);
             this.pay_btn.TabIndex = 25;
@@ -61,7 +63,7 @@
             // 
             this.lb_cost_without_tax.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_cost_without_tax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_cost_without_tax.Location = new System.Drawing.Point(333, 259);
+            this.lb_cost_without_tax.Location = new System.Drawing.Point(333, 300);
             this.lb_cost_without_tax.Name = "lb_cost_without_tax";
             this.lb_cost_without_tax.Size = new System.Drawing.Size(150, 28);
             this.lb_cost_without_tax.TabIndex = 24;
@@ -72,7 +74,7 @@
             // 
             this.lb_vat.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_vat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_vat.Location = new System.Drawing.Point(333, 209);
+            this.lb_vat.Location = new System.Drawing.Point(333, 250);
             this.lb_vat.Name = "lb_vat";
             this.lb_vat.Size = new System.Drawing.Size(150, 28);
             this.lb_vat.TabIndex = 21;
@@ -83,7 +85,7 @@
             // 
             this.lb_create_at.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_create_at.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_create_at.Location = new System.Drawing.Point(333, 159);
+            this.lb_create_at.Location = new System.Drawing.Point(333, 200);
             this.lb_create_at.Name = "lb_create_at";
             this.lb_create_at.Size = new System.Drawing.Size(150, 28);
             this.lb_create_at.TabIndex = 20;
@@ -94,7 +96,7 @@
             // 
             this.lb_invoice_id.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_invoice_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_invoice_id.Location = new System.Drawing.Point(333, 109);
+            this.lb_invoice_id.Location = new System.Drawing.Point(333, 150);
             this.lb_invoice_id.Name = "lb_invoice_id";
             this.lb_invoice_id.Size = new System.Drawing.Size(150, 28);
             this.lb_invoice_id.TabIndex = 18;
@@ -105,18 +107,18 @@
             // 
             this.lb_invoice_id_text.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_invoice_id_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_invoice_id_text.Location = new System.Drawing.Point(552, 109);
+            this.lb_invoice_id_text.Location = new System.Drawing.Point(552, 150);
             this.lb_invoice_id_text.Name = "lb_invoice_id_text";
             this.lb_invoice_id_text.Size = new System.Drawing.Size(100, 28);
             this.lb_invoice_id_text.TabIndex = 27;
-            this.lb_invoice_id_text.Text = "0000000";
+            this.lb_invoice_id_text.Text = "1";
             this.lb_invoice_id_text.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lb_create_at_text
             // 
             this.lb_create_at_text.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_create_at_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_create_at_text.Location = new System.Drawing.Point(552, 159);
+            this.lb_create_at_text.Location = new System.Drawing.Point(552, 200);
             this.lb_create_at_text.Name = "lb_create_at_text";
             this.lb_create_at_text.Size = new System.Drawing.Size(100, 28);
             this.lb_create_at_text.TabIndex = 28;
@@ -127,7 +129,7 @@
             // 
             this.lb_vat_text.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_vat_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_vat_text.Location = new System.Drawing.Point(552, 209);
+            this.lb_vat_text.Location = new System.Drawing.Point(552, 250);
             this.lb_vat_text.Name = "lb_vat_text";
             this.lb_vat_text.Size = new System.Drawing.Size(100, 28);
             this.lb_vat_text.TabIndex = 29;
@@ -138,7 +140,7 @@
             // 
             this.lb_cost_without_tax_text.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_cost_without_tax_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_cost_without_tax_text.Location = new System.Drawing.Point(552, 259);
+            this.lb_cost_without_tax_text.Location = new System.Drawing.Point(552, 300);
             this.lb_cost_without_tax_text.Name = "lb_cost_without_tax_text";
             this.lb_cost_without_tax_text.Size = new System.Drawing.Size(100, 28);
             this.lb_cost_without_tax_text.TabIndex = 30;
@@ -149,7 +151,7 @@
             // 
             this.lb_total_cost.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_total_cost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_total_cost.Location = new System.Drawing.Point(333, 309);
+            this.lb_total_cost.Location = new System.Drawing.Point(333, 350);
             this.lb_total_cost.Name = "lb_total_cost";
             this.lb_total_cost.Size = new System.Drawing.Size(150, 28);
             this.lb_total_cost.TabIndex = 31;
@@ -160,7 +162,7 @@
             // 
             this.lb_total_cost_text.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_total_cost_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_total_cost_text.Location = new System.Drawing.Point(552, 309);
+            this.lb_total_cost_text.Location = new System.Drawing.Point(552, 350);
             this.lb_total_cost_text.Name = "lb_total_cost_text";
             this.lb_total_cost_text.Size = new System.Drawing.Size(100, 28);
             this.lb_total_cost_text.TabIndex = 32;
@@ -171,7 +173,7 @@
             // 
             this.lb_amount_receiced.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_amount_receiced.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_amount_receiced.Location = new System.Drawing.Point(333, 359);
+            this.lb_amount_receiced.Location = new System.Drawing.Point(333, 400);
             this.lb_amount_receiced.Name = "lb_amount_receiced";
             this.lb_amount_receiced.Size = new System.Drawing.Size(150, 28);
             this.lb_amount_receiced.TabIndex = 33;
@@ -182,7 +184,7 @@
             // 
             this.tb_description.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.tb_description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_description.Location = new System.Drawing.Point(552, 359);
+            this.tb_description.Location = new System.Drawing.Point(552, 400);
             this.tb_description.Name = "tb_description";
             this.tb_description.Size = new System.Drawing.Size(100, 27);
             this.tb_description.TabIndex = 34;
@@ -192,7 +194,7 @@
             // 
             this.lb_change.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_change.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_change.Location = new System.Drawing.Point(333, 407);
+            this.lb_change.Location = new System.Drawing.Point(333, 450);
             this.lb_change.Name = "lb_change";
             this.lb_change.Size = new System.Drawing.Size(150, 28);
             this.lb_change.TabIndex = 35;
@@ -203,7 +205,7 @@
             // 
             this.lb_change_text.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lb_change_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_change_text.Location = new System.Drawing.Point(548, 407);
+            this.lb_change_text.Location = new System.Drawing.Point(548, 450);
             this.lb_change_text.Name = "lb_change_text";
             this.lb_change_text.Size = new System.Drawing.Size(100, 28);
             this.lb_change_text.TabIndex = 36;
@@ -221,13 +223,34 @@
             this.label1.TabIndex = 37;
             this.label1.Text = "Hóa đơn bán hàng";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            
             // 
-            // Invoice
+            // lb_checkout_id_text
+            // 
+            this.lb_checkout_id_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_checkout_id_text.Location = new System.Drawing.Point(548, 100);
+            this.lb_checkout_id_text.Name = "lb_checkout_id_text";
+            this.lb_checkout_id_text.Size = new System.Drawing.Size(100, 28);
+            this.lb_checkout_id_text.TabIndex = 39;
+            this.lb_checkout_id_text.Text = "1";
+            this.lb_checkout_id_text.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lb_checkout_id
+            // 
+            this.lb_checkout_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_checkout_id.Location = new System.Drawing.Point(333, 100);
+            this.lb_checkout_id.Name = "lb_checkout_id";
+            this.lb_checkout_id.Size = new System.Drawing.Size(150, 28);
+            this.lb_checkout_id.TabIndex = 38;
+            this.lb_checkout_id.Text = "Mã Checkout:";
+            this.lb_checkout_id.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // GUI_Invoice
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1006, 529);
+            this.Controls.Add(this.lb_checkout_id_text);
+            this.Controls.Add(this.lb_checkout_id);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lb_change_text);
             this.Controls.Add(this.lb_change);
@@ -244,7 +267,7 @@
             this.Controls.Add(this.lb_vat);
             this.Controls.Add(this.lb_create_at);
             this.Controls.Add(this.lb_invoice_id);
-            this.Name = "Invoice";
+            this.Name = "GUI_Invoice";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hóa đơn";
             this.Load += new System.EventHandler(this.invoice_Load);
@@ -270,5 +293,7 @@
         private System.Windows.Forms.Label lb_change;
         private System.Windows.Forms.Label lb_change_text;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_checkout_id_text;
+        private System.Windows.Forms.Label lb_checkout_id;
     }
 }
--- INFSYS_Design/views/room_status.Designer.cs
+++ INFSYS_Design/views/room_status.Designer.cs
@@ -36,6 +36,8 @@
             this.lb_device_name = new System.Windows.Forms.Label();
             this.tb_device_name = new System.Windows.Forms.TextBox();
             this.tb_compensation_cost = new System.Windows.Forms.TextBox();
+            this.lb_checkout_id = new System.Windows.Forms.Label();
+            this.lb_checkout_id_text = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // create_invoice_btn
@@ -43,7 +45,7 @@
             this.create_invoice_btn.Location = new System.Drawing.Point(353, 270);
             this.create_invoice_btn.Name = "create_invoice_btn";
             this.create_invoice_btn.Size = new System.Drawing.Size(117, 29);
-            this.create_invoice_btn.TabIndex = 26;
+            this.create_invoice_btn.TabIndex = 5;
             this.create_invoice_btn.Text = "Tạo hóa đơn";
             this.create_invoice_btn.UseVisualStyleBackColor = true;
             this.create_invoice_btn.Click += new System.EventHandler(this.create_invoice_btn_Click);
@@ -53,72 +55,94 @@
             this.add_btn.Location = new System.Drawing.Point(117, 270);
             this.add_btn.Name = "add_btn";
             this.add_btn.Size = new System.Drawing.Size(97, 29);
-            this.add_btn.TabIndex = 25;
+            this.add_btn.TabIndex = 4;
             this.add_btn.Text = "Thêm";
             this.add_btn.UseVisualStyleBackColor = true;
             // 
             // lb__compensation_cost
             // 
             this.lb__compensation_cost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb__compensation_cost.Location = new System.Drawing.Point(40, 173);
+            this.lb__compensation_cost.Location = new System.Drawing.Point(40, 200);
             this.lb__compensation_cost.Name = "lb__compensation_cost";
             this.lb__compensation_cost.Size = new System.Drawing.Size(150, 28);
             this.lb__compensation_cost.TabIndex = 21;
-            this.lb__compensation_cost.Text = "Chi phí đền bù";
+            this.lb__compensation_cost.Text = "Chi phí đền bù:";
             this.lb__compensation_cost.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lb_description
             // 
             this.lb_description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_description.Location = new System.Drawing.Point(40, 111);
+            this.lb_description.Location = new System.Drawing.Point(40, 150);
             this.lb_description.Name = "lb_description";
             this.lb_description.Size = new System.Drawing.Size(150, 28);
             this.lb_description.TabIndex = 20;
-            this.lb_description.Text = "Mô tả tình trạng";
+            this.lb_description.Text = "Mô tả tình trạng:";
             this.lb_description.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // tb_description
             // 
             this.tb_description.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_description.Location = new System.Drawing.Point(200, 111);
+            this.tb_description.Location = new System.Drawing.Point(200, 150);
             this.tb_description.Multiline = true;
             this.tb_description.Name = "tb_description";
             this.tb_description.Size = new System.Drawing.Size(325, 28);
-            this.tb_description.TabIndex = 19;
+            this.tb_description.TabIndex = 2;
             // 
             // lb_device_name
             // 
             this.lb_device_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_device_name.Location = new System.Drawing.Point(40, 52);
+            this.lb_device_name.Location = new System.Drawing.Point(40, 100);
             this.lb_device_name.Name = "lb_device_name";
             this.lb_device_name.Size = new System.Drawing.Size(150, 28);
             this.lb_device_name.TabIndex = 18;
-            this.lb_device_name.Text = "Tên thiết bị";
+            this.lb_device_name.Text = "Tên thiết bị:";
             this.lb_device_name.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // tb_device_name
             // 
             this.tb_device_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_device_name.Location = new System.Drawing.Point(200, 52);
+            this.tb_device_name.Location = new System.Drawing.Point(200, 100);
             this.tb_device_name.Multiline = true;
             this.tb_device_name.Name = "tb_device_name";
             this.tb_device_name.Size = new System.Drawing.Size(325, 28);
-            this.tb_device_name.TabIndex = 17;
+            this.tb_device_name.TabIndex = 1;
             // 
             // tb_compensation_cost
             // 
             this.tb_compensation_cost.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_compensation_cost.Location = new System.Drawing.Point(200, 173);
+            this.tb_compensation_cost.Location = new System.Drawing.Point(200, 200);
             this.tb_compensation_cost.Multiline = true;
             this.tb_compensation_cost.Name = "tb_compensation_cost";
             this.tb_compensation_cost.Size = new System.Drawing.Size(325, 28);
-            this.tb_compensation_cost.TabIndex = 27;
+            this.tb_compensation_cost.TabIndex = 3;
             // 
-            // RoomStatus
+            // lb_checkout_id
+            // 
+            this.lb_checkout_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_checkout_id.Location = new System.Drawing.Point(40, 50);
+            this.lb_checkout_id.Name = "lb_checkout_id";
+            this.lb_checkout_id.Size = new System.Drawing.Size(150, 28);
+            this.lb_checkout_id.TabIndex = 28;
+            this.lb_checkout_id.Text = "Mã Checkout:";
+            this.lb_checkout_id.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lb_checkout_id_text
+            // 
+            this.lb_checkout_id_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_checkout_id_text.Location = new System.Drawing.Point(200, 50);
+            this.lb_checkout_id_text.Name = "lb_checkout_id_text";
+            this.lb_checkout_id_text.Size = new System.Drawing.Size(150, 28);
+            this.lb_checkout_id_text.TabIndex = 29;
+            this.lb_checkout_id_text.Text = "1";
+            this.lb_checkout_id_text.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // GUI_RoomStatus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 353);
+            this.Controls.Add(this.lb_checkout_id_text);
+            this.Controls.Add(this.lb_checkout_id);
             this.Controls.Add(this.tb_compensation_cost);
             this.Controls.Add(this.create_invoice_btn);
             this.Controls.Add(this.add_btn);
@@ -127,7 +151,7 @@
             this.Controls.Add(this.tb_description);
             this.Controls.Add(this.lb_device_name);
             this.Controls.Add(this.tb_device_name);
-            this.Name = "RoomStatus";
+            this.Name = "GUI_RoomStatus";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tình trạng phòng";
             this.Load += new System.EventHandler(this.room_status_Load);
@@ -146,5 +170,7 @@
         private System.Windows.Forms.Label lb_device_name;
         private System.Windows.Forms.TextBox tb_device_name;
         private System.Windows.Forms.TextBox tb_compensation_cost;
+        private System.Windows.Forms.Label lb_checkout_id;
+        private System.Windows.Forms.Label lb_checkout_id_text;
     }
 }
